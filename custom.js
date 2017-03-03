6a20ce9008a0f5336c03ef1fd62cab0453ca8369
IagoLast <iago.lastra@gmail.com>
2017-03-03 22:53:37 +0100

    c7
    Add attributes support

--- custom.js
+++ custom.js
@@ -9,6 +9,14 @@ class PElement extends HTMLElement {
     return '';
   }
 
+  get attrs() {
+    let attrs = {}
+    for (let i = 0; i < this.attributes.length; i++) {
+      attrs[this.attributes[i].nodeName] = this.attributes[i].nodeValue;
+    }
+    return attrs;
+  }
+
   render() {
     this.innerHTML = this.template;
   }
--- hello.js
+++ hello.js
@@ -1,32 +1,9 @@
 class Hello extends PElement {
-  constructor() {
-    super();
-    this.state.text = '';
-  }
 
   get template() {
-    return `
-      <section>
-        <h1>Data binding</h1>
-        <input type="text" name="bind" />
-        <p>${this.state.text}</p>
-      </section>
-    `;
+    return `<img width="400" src="${this.attrs.src}" alt="Cute Cat"> `;
   }
 
-  connectedCallback() {
-    this.innerHTML = this.template;
-    this.querySelector('input').addEventListener('input', this.onChange.bind(this));
-  }
-
-  onChange(event) {
-    this.state.text = event.target.value
-    this.setState(this.state)
-  }
-
-  render() {
-    this.querySelector('p').innerHTML = this.state.text;
-  }
 }
 
 customElements.define('hello-world', Hello);
