65d1a67e681c648b997310009192050ad43ba471
IagoLast <iago.lastra@gmail.com>
2017-03-03 22:55:47 +0100

    c8
    Example of repeater
    Example of filtering

--- hello.js
+++ hello.js
@@ -1,7 +1,35 @@
 class Hello extends PElement {
 
+  constructor() {
+    super();
+    this.state.elements = ['c', 'c++', 'python', 'java', 'javascript', 'ocaml'];
+  }
+
   get template() {
-    return `<img width="400" src="${this.attrs.src}" alt="Cute Cat"> `;
+    return `
+      <h1>List filtering</h1>
+      <input type="text">
+      <div class="items">
+        ${this.state.elements.map(element => `<p> ${element} </p> `).join('')}
+      </div>
+    `;
+  }
+
+  connectedCallback() {
+    this.innerHTML = this.template;
+    this.querySelector('input').addEventListener('input', this.onChange.bind(this));
+  }
+
+  render() {
+    let query = this.state.query;
+    let elements = this.state.elements;
+    let filtered = query ? elements.filter(item => item.includes(query)) : elements;
+    this.querySelector('div.items').innerHTML = filtered.map(element => `<p> ${element}  </p> `).join('');
+  }
+
+  onChange() {
+    this.state.query = event.target.value
+    this.setState(this.state)
   }
 
 }
